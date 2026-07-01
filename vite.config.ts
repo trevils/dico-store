1ec0a06c65ef698e75efaa1e4864489032a5073d
trevils <petrov.n01@mail.ru>
2026-07-02 01:31:52 +0700

    GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1];
+const isGitHubPagesBuild = process.env.GITHUB_ACTIONS === 'true' && repositoryName;
+
 export default defineConfig({
-  base: './',
+  base: isGitHubPagesBuild ? `/${repositoryName}/` : '/',
   plugins: [react()],
 });
